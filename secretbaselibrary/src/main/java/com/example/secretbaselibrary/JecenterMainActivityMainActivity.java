77476297216285b87d300ee1b2a1136656368256
secretbase <838514984@qq.com>
2017-08-31 11:31:15 +0800

    新增上传jecenter！！！ 主要在local.property中配置用户名和apikey 在moudle.gradle中配置信息，这里注意artificname要和library的名字一样！！！ 还有jecenter的账号注册问题，很是坑爹。不能注册organize的账号，要个人的，不然上传没有权限！ 其他就照抄了。没有问题。

---
+++ secretbaselibrary/src/main/java/com/example/secretbaselibrary/JecenterMainActivityMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.secretbaselibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JecenterMainActivityMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secret_activity_main);
+    }
+}
